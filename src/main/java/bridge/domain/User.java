60ad6ecd702e9904c272805caaa61917a808bed0
kkm5291 <kkm23125291@gmail.com>
2023-03-15 19:44:29 +0900

    feat(User) : add move method, validateStatus method, isNotStatus method

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -1,6 +1,9 @@
 package bridge.domain;
 
 public class User {
+
+    private static final String UP_STAIR = "U";
+    private static final String DOWN_STAIR = "D";
     private int step = 0;
     private boolean live = true;
 
@@ -9,8 +12,23 @@ public class User {
         this.live = true;
     }
 
-    public void move() {
+    public void move(Bridge bridge, String input) {
+        validateStatus(input);
+        if (!bridge.isSafe(step, input)) {
+            live = false;
+            return;
+        }
+        step++;
+    }
 
+    private void validateStatus(String status) {
+        if (isNotStatus(status)) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    private boolean isNotStatus(String status) {
+        return !UP_STAIR.equals(status) && !DOWN_STAIR.equals(status);
     }
 
     public int getStep() {
